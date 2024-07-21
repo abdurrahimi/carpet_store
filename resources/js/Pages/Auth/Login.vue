8a5bce20472959cc80e5c259d617feb20d380bf9
Abdur Rahimi <abdurrahimi22@gmail.com>
2024-07-22 01:37:47 +0700

    chore: grunt task

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -88,10 +88,10 @@ const submit = () => {
         </PrimaryButton>
       </div>
     </form> -->
-    <div class="bg-gradient-primary">
+    <div class="bg-gradient-primary" style="height:100vh">
       <div class="container">
         <!-- Outer Row -->
-        <div class="row justify-content-center">
+        <div class="row justify-content-center align-items-center" style="height:100vh">
           <div class="col-xl-10 col-lg-12 col-md-9">
             <div class="card o-hidden border-0 shadow-lg my-5">
               <div class="card-body p-0">
