ad0f4e591c584b0f3ad467ea3663f1d479612858
Abdur Rahimi <abdurrahimi22@gmail.com>
2025-07-09 16:08:56 +0700

    up

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,15 +10,15 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="{{ asset('/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,400,1,200" />
     <!-- Custom styles for this template-->
-    <link href="/css/sb-admin-2.css" rel="stylesheet">
-    <link rel="stylesheet" href="/css/icon.css" />
+    <link href="{{ asset('/css/sb-admin-2.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('/css/icon.css')}}" />
     <!-- Scripts -->
     @routes
     @vite(['public/js/sb-admin-2.js', 'resources/js/app.jsx', "resources/js/Pages/{$page['component']}.vue"])
@@ -59,11 +59,11 @@
 <body class="font-sans antialiased">
     @inertia
 </body>
-<script src="/vendor/jquery/jquery.min.js"></script>
-<script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('/vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{ asset('/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
 <!-- Core plugin JavaScript-->
-<script src="/vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="{{ asset('/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
 
 <!-- Custom scripts for all pages-->
 {{-- <script src="js/sb-admin-2.js" defer></script> --}}
